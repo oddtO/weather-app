4aa22071b84e581e19ba2d48fa84ba047d6b99e8
oddtO <121516833+oddtO@users.noreply.github.com>
2023-10-10 01:37:42 +0300

    style header

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 import "../css/reset.css";
 import "../css/style.css";
+import "../css/body.css";
+import "../css/header-body.css";
+import "../css/media.css";
 import { askCurWeatherForACity } from "./api/askCurWeatherForACity.js";
 import { askTodayWeatherStatistics } from "./api/askTodayWeatherStatistics.js";
-console.log("cur", await askCurWeatherForACity("Cherkasy"));
-console.log("his", await askTodayWeatherStatistics("Cherkasy"));
+
+/* console.log("cur", await askCurWeatherForACity("Cherkasy"));
+console.log("his", await askTodayWeatherStatistics("Cherkasy")); */
