cb340031fce4180d33074334e27b10573f7b475c
oddtO <121516833+oddtO@users.noreply.github.com>
2023-10-12 04:53:07 +0300

    set up main weather data display

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,9 @@ import "../css/style.css";
 import "../css/body.css";
 import "../css/header-body.css";
 import "../css/media.css";
+import "../css/main-body.css";
 import { askCurWeatherForACity } from "./api/askCurWeatherForACity.js";
 import { askTodayWeatherStatistics } from "./api/askTodayWeatherStatistics.js";
 
-/* console.log("cur", await askCurWeatherForACity("Cherkasy"));
-console.log("his", await askTodayWeatherStatistics("Cherkasy")); */
+console.log("cur", await askCurWeatherForACity("Cherkasy"));
+console.log("his", await askTodayWeatherStatistics("Cherkasy"));
