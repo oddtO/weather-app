7337d84ec723ca65a6ae270004cba28a5b9d4087
oddtO <121516833+oddtO@users.noreply.github.com>
2023-10-15 01:10:24 +0300

    make hours today table responsive

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ import "../css/body.css";
 import "../css/header-body.css";
 import "../css/media.css";
 import "../css/main-body.css";
+import "../css/hours_detailed-main.css";
+import "../css/next_days-main.css";
 import { askCurWeatherForACity } from "./api/askCurWeatherForACity.js";
 import { askTodayWeatherStatistics } from "./api/askTodayWeatherStatistics.js";
 
