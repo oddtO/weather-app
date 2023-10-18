638c87046f2f99874829f814da85414533d9121e
oddtO <121516833+oddtO@users.noreply.github.com>
2023-10-18 17:29:44 +0300

    add loading component

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,7 @@ import "../css/media.css";
 import "../css/main-body.css";
 import "../css/hours_detailed-main.css";
 import "../css/next_days-main.css";
+import "../css/loading.css";
 import { askCurWeatherForACity } from "./api/askCurWeatherForACity.js";
 import { askTodayWeatherStatistics } from "./api/askTodayWeatherStatistics.js";
 
